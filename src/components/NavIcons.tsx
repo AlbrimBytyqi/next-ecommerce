6c556814e05c68b39577845812cbca740c71b935
Albrim Bytyqi <bytyqialbrimaia@gmail.com>
2025-05-03 17:46:13 +0200

    Next.js Authentication with Wix Headless (Email)

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -2,58 +2,75 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import React, { useState } from "react";
 import CartModel from "./CartModel";
 import { useWixClient } from "@/hooks/useWixClient";
+import Cookies from "js-cookie";
 
 const NavIcons = () => {
   const [isProfileOpen, setIsProfileOpen] = useState(false);
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const router = useRouter();
+  const pathName = usePathname();
 
-  //TEMPORANEO
-  const IsLoggedIn = false;
+  const wixClient = useWixClient();
+  const IsLoggedIn = wixClient.auth.loggedIn();
 
   const handleProfile = () => {
     if (!IsLoggedIn) {
       router.push("/login");
+    } else {
+      setIsProfileOpen((prev) => !prev);
     }
-    setIsProfileOpen((prev) => !prev);
   };
 
-  // AUTH WITH WIX-MANAGED AUTH
+  // // AUTH WITH WIX-MANAGED AUTH
 
-  const wixClient = useWixClient();
+  // const wixClient = useWixClient();
 
-  const login = async () => {
-    const loginRequestData = wixClient.auth.generateOAuthData(
-      "http://localhost:3000"
-    );
+  // const login = async () => {
+  //   const loginRequestData = wixClient.auth.generateOAuthData(
+  //     "http://localhost:3000"
+  //   );
 
-    console.log(loginRequestData);
+  //   console.log(loginRequestData);
 
-    localStorage.setItem("oAuthRedirectData", JSON.stringify(loginRequestData));
-    const { authUrl } = await wixClient.auth.getAuthUrl(loginRequestData);
-    window.location.href = authUrl;
+  //   localStorage.setItem("oAuthRedirectData", JSON.stringify(loginRequestData));
+  //   const { authUrl } = await wixClient.auth.getAuthUrl(loginRequestData);
+  //   window.location.href = authUrl;
+  // };
+
+  console.log(pathName);
+
+  const handleLogout = async () => {
+    setIsLoading(true);
+    Cookies.remove("refreshToken");
+    const { logoutUrl } = await wixClient.auth.logout(window.location.href);
+    setIsLoading(false);
+    setIsProfileOpen(false);
+    router.push(logoutUrl);
   };
 
   return (
     <div className="flex items-center gap-4 xl:gap-6 relative">
       <Image
-        src={"/profile.png"}
+        src="/profile.png"
         alt=""
         width={22}
         height={22}
         className="cursor-pointer"
-        // onClick={handleProfile}
-        onClick={login}
+        // onClick={login}
+        onClick={handleProfile}
       />
       {isProfileOpen && (
-        <div className="absolute p-4 rounded-md top-12 left-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
-          <Link href={""}>Profile</Link>
-          <div className="mt-2 cursor-pointer"> Logout</div>
+        <div className="absolute p-4 rounded-md top-12 left-0 bg-white text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
+          <Link href="/profile">Profile</Link>
+          <div className="mt-2 cursor-pointer" onClick={handleLogout}>
+            {isLoading ? "Logging out" : "Logout"}
+          </div>
         </div>
       )}
       <Image
